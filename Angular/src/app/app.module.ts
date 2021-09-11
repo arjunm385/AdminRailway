f3de8fc54ec42aeb16b42a6367ffaa1e98d70728
arjunm385 <arjunm385@gmail.com>
2021-09-11 18:21:56 +0530

    updated search train

--- Angular/src/app/app.module.ts
+++ Angular/src/app/app.module.ts
@@ -6,11 +6,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TrainformComponent } from './trainform/trainform.component';
+import { SearchtrainComponent } from './searchtrain/searchtrain.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    TrainformComponent
+    TrainformComponent,
+    SearchtrainComponent
   ],
   imports: [
     BrowserModule,
